a569c70a87e9e68fe635c0aec64a4e233102be68
fragolan01 <157424555+fragolan01@users.noreply.github.com>
2024-03-02 12:31:48 -0600

    TABLA TEMPORA/TIPO DE CAMBIO
    TIPO DE CAMBIO

--- fragolan/ejemplo_orden.php
+++ fragolan/ejemplo_orden.php
@@ -8,12 +8,18 @@ $archivo = 'lista_ids.txt';
 // Abrir el archivo en modo lectura
  $manejador = fopen($archivo, 'r',FILE_IGNORE_NEW_LINES);
 
+// Dolar
+$dolar = 0.0;
+
 
 // Establece el límite de tiempo a 300 segundos (5 minutos)
 set_time_limit(300); 
 
 // Token de autenticación
 $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjM1NmU3OTkwNmJkYjJjYWNhYTJjMWM5MjZmZGNjM2M4ZmEzNzQ4ZGY0Y2VjZWUxOGQzMWFlY2Q3MWViODJmMjFmMWY3ZDBhMGJlZDk1NzkxIn0.eyJhdWQiOiJ5ZmQwS1g4U1REYUtPZEJ0cHB2UG4wSWVFeUdiVW1CVCIsImp0aSI6IjM1NmU3OTkwNmJkYjJjYWNhYTJjMWM5MjZmZGNjM2M4ZmEzNzQ4ZGY0Y2VjZWUxOGQzMWFlY2Q3MWViODJmMjFmMWY3ZDBhMGJlZDk1NzkxIiwiaWF0IjoxNzA2NTUxMzA3LCJuYmYiOjE3MDY1NTEzMDcsImV4cCI6MTczODA4NzMwNiwic3ViIjoiIiwic2NvcGVzIjpbXX0.jhALtrRj_tkgNVj6CZxuEAnWxG6qpUMeOrXZvRbLU7B5prHrc-zPmn4lLcaEDDgfWRTXHEyQrN1nRpO8EQLuBug1kUJm-mwCkPhFMb4U6c7u_S4O0WWB4bNrRv_CQpz1Vdvic1pIJB5PDurPrzG2KbHlzfogdeYWolCKFShqPH5eehoJ0MwJ5AlL83AqpFhqzeprjB0K9eGJMx3a5jc8fYZxQm7jgh1uNk4LfaapuMos23IWczeC_1uQ3Y1XW1yuYaHXY5f9N5RA_IfBULEQ-ya8UL7Bem1ntWRegx1oIQ2M1sGz5hsdyiepI313K61rGa9khk_wI9bmwBwHxca4X_sIMT_sdJ9yOVzgXMRFfG-QlvhNWK-4xDldbo52uYwxu094cwTFZijk9NmNQq-WfPNyHEzmBrL7lSmuPVSqokggA0LjvHPnXmYCz30NxonC-zSgVp_SEBcF7rw0qo5oKe7VDj0GmPHeNV9T1n8IfFo7LaALHfyw4KAwivecMh9XY5GC_IYBLWrjAwqystUW2uiVS660t7mDqvfKonFjgjZyVuakVU4MDBXOJEzF9FVahBUc_MqXVvWbiYWDtVCnzj6rwiaXzLplEFnH4ntsCveizJmcQCF-hPRKHKprEJQFfN7E1TK3kWM0Mfei_URjiklr1J0lR6NmsSvF-q165mE";
+
+// url tipo de cambio
+$tipo_de_cambio = "https://developers.syscom.mx/api/v1/tipocambio";
 
 // Configurar opciones para la solicitud HTTP
 $options = array(
@@ -22,6 +28,35 @@ $options = array(
         'method'  => 'GET'
     )
 );
+
+
+// Crear contexto de flujo
+$context = stream_context_create($options);
+
+
+// Realizar la consulta a la API con el token de autenticación
+$response = file_get_contents($tipo_de_cambio, false, $context);
+
+
+// Verificar si la consulta fue exitosa
+if ($response === FALSE) {
+    // Manejar el error si la consulta falla
+    $result = array('error' => 'Error al consultar la API SYSCOM');
+} else {
+    // Procesar los datos recibidos (en este ejemplo asumimos que la respuesta es en JSON)
+    $data = json_decode($response, true);
+
+    // Verificar si la decodificación tuvo éxito
+    if ($data === null) {
+        die('Error al decodificar el JSON');
+    }
+
+    // Acceder a los datos
+    echo "TIPO DE CAMBIO: ".$data['normal']."<br><br> ";
+
+
+}
+
 
 
 // Verificar si el archivo se abrió correctamente
@@ -87,7 +122,6 @@ if ($manejador) {
                     }
                     // mas datos
                     echo "<br>";
-
 
                 }
             }
