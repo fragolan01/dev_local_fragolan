6f5ead148ca5c490905c6f8d1d68b226beb1b8f3
fragolan01 <157424555+fragolan01@users.noreply.github.com>
2024-03-04 12:40:36 -0600

    Inserta db
    Inserta datos en bd local, pendiente imgresar datatime

--- fragolan/ejemplo_orden.php
+++ fragolan/ejemplo_orden.php
@@ -1,15 +1,32 @@
 <?php
+
+
+$servername = "localhost"; // Servidor de base de datos
+$username = "root"; // Usuario de MySQL
+$password = ""; // Contraseña de MySQL
+$database = "fragcom_develop"; // base de datos
+
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+
+
 // Fecha
 $fecha = new DateTime();
-
-// Ruta del archivo .txt
+// date_default_timezone_set('');
+// Archivo .txt
 $archivo = 'lista_ids.txt';
-
 // Abrir el archivo en modo lectura
  $manejador = fopen($archivo, 'r',FILE_IGNORE_NEW_LINES);
-
 // Dolar
 $dolar = 0.0;
+
+$id_dominio=9999;
 
 
 // Establece el límite de tiempo a 300 segundos (5 minutos)
@@ -17,10 +34,8 @@ set_time_limit(300);
 
 // Token de autenticación
 $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjM1NmU3OTkwNmJkYjJjYWNhYTJjMWM5MjZmZGNjM2M4ZmEzNzQ4ZGY0Y2VjZWUxOGQzMWFlY2Q3MWViODJmMjFmMWY3ZDBhMGJlZDk1NzkxIn0.eyJhdWQiOiJ5ZmQwS1g4U1REYUtPZEJ0cHB2UG4wSWVFeUdiVW1CVCIsImp0aSI6IjM1NmU3OTkwNmJkYjJjYWNhYTJjMWM5MjZmZGNjM2M4ZmEzNzQ4ZGY0Y2VjZWUxOGQzMWFlY2Q3MWViODJmMjFmMWY3ZDBhMGJlZDk1NzkxIiwiaWF0IjoxNzA2NTUxMzA3LCJuYmYiOjE3MDY1NTEzMDcsImV4cCI6MTczODA4NzMwNiwic3ViIjoiIiwic2NvcGVzIjpbXX0.jhALtrRj_tkgNVj6CZxuEAnWxG6qpUMeOrXZvRbLU7B5prHrc-zPmn4lLcaEDDgfWRTXHEyQrN1nRpO8EQLuBug1kUJm-mwCkPhFMb4U6c7u_S4O0WWB4bNrRv_CQpz1Vdvic1pIJB5PDurPrzG2KbHlzfogdeYWolCKFShqPH5eehoJ0MwJ5AlL83AqpFhqzeprjB0K9eGJMx3a5jc8fYZxQm7jgh1uNk4LfaapuMos23IWczeC_1uQ3Y1XW1yuYaHXY5f9N5RA_IfBULEQ-ya8UL7Bem1ntWRegx1oIQ2M1sGz5hsdyiepI313K61rGa9khk_wI9bmwBwHxca4X_sIMT_sdJ9yOVzgXMRFfG-QlvhNWK-4xDldbo52uYwxu094cwTFZijk9NmNQq-WfPNyHEzmBrL7lSmuPVSqokggA0LjvHPnXmYCz30NxonC-zSgVp_SEBcF7rw0qo5oKe7VDj0GmPHeNV9T1n8IfFo7LaALHfyw4KAwivecMh9XY5GC_IYBLWrjAwqystUW2uiVS660t7mDqvfKonFjgjZyVuakVU4MDBXOJEzF9FVahBUc_MqXVvWbiYWDtVCnzj6rwiaXzLplEFnH4ntsCveizJmcQCF-hPRKHKprEJQFfN7E1TK3kWM0Mfei_URjiklr1J0lR6NmsSvF-q165mE";
-
 // url tipo de cambio
 $tipo_de_cambio = "https://developers.syscom.mx/api/v1/tipocambio";
-
 // Configurar opciones para la solicitud HTTP
 $options = array(
     'http' => array(
@@ -32,8 +47,6 @@ $options = array(
 
 // Crear contexto de flujo
 $context = stream_context_create($options);
-
-
 // Realizar la consulta a la API con el token de autenticación
 $response = file_get_contents($tipo_de_cambio, false, $context);
 
@@ -54,9 +67,7 @@ if ($response === FALSE) {
     // Acceder a los datos
     echo "TIPO DE CAMBIO: ".$data['normal']."<br><br> ";
 
-
 }
-
 
 
 // Verificar si el archivo se abrió correctamente
@@ -112,19 +123,23 @@ if ($manejador) {
                     $stock = ['total_existencia'];
                     // echo gettype($stock);
 
-                    //Converit a integer 
+                    //Converit a integer las varibales
+                    $int_orden = intval($orden);
+                    $int_producto_id = intval($data['producto_id']);
                     $int_stock = intval($data['total_existencia']);
+                    $int_inv_minimo = intval($ìnv_minimo); 
 
+                    // Valida producto ACTIVO o en PAUSA
                     if($int_stock < $ìnv_minimo){
+                        $status = 0;
                         echo 'PAUSA'.'<br>';
                     }else{
+                        $status = 1;
                         echo 'ACTIVO'.'<br>';
                     }
 
-
-
     
-                    echo "PRODUCTO-ID: ".$data['producto_id']."<br>";
+                    echo "ID: ".$data['producto_id']."<br>";
                     echo "STOCK: ".$data['total_existencia']."<br>";
                     echo 'INV. MINI: '.$ìnv_minimo.'<br>';
 
@@ -132,16 +147,28 @@ if ($manejador) {
                     // ***PRECIO
                     if (isset($precio_descuento)) {
                         echo "PRECIO: " . $precio_descuento.'<br>';
-                        // echo $fecha->format('U = Y-m-d H:i:s');
+                        $fecha->format('Y-m-d H:i:s');
                         echo "<br>";
 
                     }
-                    // mas datos
-                    echo "<br>";
+
+                    //Converit a integer las varibales
+                    $int_precio_descuento = intval($precio_descuento);
+
+
+                    //Insertando datos
+                    $sql = "INSERT INTO plataforma_ventas_temp (id_dominio, id_syscom, orden, stock, precio, inv_min, status) 
+                    VALUES ('$id_dominio', '$int_producto_id', '$int_orden', '$int_stock','$int_precio_descuento','$int_inv_minimo', '$status')";
+            
+
+                    if ($conn->query($sql) === TRUE) {
+                            echo "\Datos insertados correctamente en la tabla.";
+                        } else {
+                            echo "Error al insertar datos: " . $conn->error;
+                        }
+                    }
 
                 }
-            }
-
             
         } else {
             // Si la línea no tiene tres partes, mostrar un mensaje de error
@@ -151,6 +178,9 @@ if ($manejador) {
     
     // Cerrar el archivo
     fclose($manejador);
+    // Cierra la BD
+    $conn->close();
+
 } else {
     // Si no se puede abrir el archivo, mostrar un mensaje de error
     echo "Error: No se pudo abrir el archivo.\n";
