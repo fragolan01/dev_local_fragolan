3c05a344d6ad358a57a99fabdeb241747f66b7c3
fragolan01 <157424555+fragolan01@users.noreply.github.com>
2024-03-28 12:44:53 -0600

    Status calculado
    Status calculado en consulta

--- fragolan/todos_los_productos.php
+++ fragolan/todos_los_productos.php
@@ -1,7 +1,9 @@
 <?php
 require_once('conexion.php');
 
-$sql = "SELECT 
+$sql = "
+
+SELECT 
 t1.orden,
 t1.fecha,
 t1.id_syscom, 
@@ -10,6 +12,7 @@ t1.stock,
 t1.inv_min, 
 t1.status, 
 t1.precio AS precio_ayer,
+IF (t1.stock <= t1.inv_min, 0, 1) AS status,
 (SELECT precio FROM plataforma_ventas_temp WHERE id_syscom = t1.id_syscom AND fecha < t1.fecha ORDER BY fecha DESC LIMIT 1) AS precio_hoy
 FROM (
 SELECT 
@@ -26,6 +29,7 @@ FROM plataforma_ventas_temp
 ) AS t1
 WHERE t1.rn = 1
 ORDER BY t1.orden;
+
 ";
 
 
@@ -43,29 +47,42 @@ if ($result->num_rows > 0) {
 
     echo "<tr>
             <th>ORDEN</th>
-            <th>FECHA</th>
-            <th>ID_SYSCOM</th>
-            <th>TITULO</th>
+            <th>FECHA CONSULTA</th>
+            <th>ID SYSCOM</th>
+            <th>NOMBRE</th>
             <th>STOCK</th>
-            <th>INV_MINIMO</th>
+            <th>INV. MINIMO</th>
             <th>STATUS</th>
-            <th>PRECIO_AYER</th>
-            <th>PRECIO_HOY</th>
+            <th>PRECIO AYER</th>
+            <th>PRECIO HOY</th>
         </tr>";
     
 
     while($row = $result->fetch_assoc()) {
+  
         echo "<tr>";    
             // Imprimir los demás datos de la fila
-            echo "<td>" . $row['orden'] . "</td>";
-            echo "<td>" . $row['fecha'] . "</td>";
-            echo "<td>" . $row['id_syscom'] . "</td>";
+            echo "<td><center>" . $row['orden'] . "</td></center>";
+            echo "<td><center>" . $row['fecha'] . "</td></center>";
+            echo "<td><center>" . $row['id_syscom'] . "</td></center>";
             echo "<td>" . $row['titulo'] . "</td>";
-            echo "<td>" . $row['stock'] . "</td>";
-            echo "<td>" . $row['inv_min'] . "</td>";
-            echo "<td>" . $row['status'] . "</td>";
-            echo "<td>" . $row['precio_ayer'] . "</td>";
-            echo "<td>" . $row['precio_hoy'] . "</td>";
+            echo "<td><center>" . $row['stock'] . "</td></center>";
+            echo "<td><center>" . $row['inv_min'] . "</td></center>";
+
+            echo "<td>"; 
+                if ($row['status'] == 1) {
+                    echo "<b><center><font color=green> ACTIVO</font></b></center>";
+        
+                } elseif ($row['status'] == 0) {
+                    echo "<b><center><font  color=red> PAUSA</font></b></center>";
+        
+                } else {
+                    echo 'Desconocido'; // Si el estado no es ni 0 ni 1
+                }
+            "</td>";
+            
+            echo "<td><center>" . $row['precio_ayer'] . "</td></center>";
+            echo "<td><center>" . $row['precio_hoy'] . "</td><center>";
 
         echo "</tr>";
     }
@@ -78,22 +95,6 @@ if ($result->num_rows > 0) {
 
 // Cierra la conexión a la base de datos
 $conn->close();
-
-
-        // Imprimir el estado
-        // echo "<td>";
-        // if ($row['status'] == 1) {
-        //     // echo 'ACTIVO';
-        //     echo "<font color=green> ACTIVO</font>";
-
-        // } elseif ($row['status'] == 0) {
-        //     echo "<b><font  color=red> PAUSA</font></b>";
-
-        // } else {
-        //     echo 'Desconocido'; // Si el estado no es ni 0 ni 1
-        // }
-        // echo "</td>";
-
 
 
 
